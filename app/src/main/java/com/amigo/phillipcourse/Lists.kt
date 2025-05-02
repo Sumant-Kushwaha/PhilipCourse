6003bcaa742e6f4b88c5a6882b91369267635fc0
Sumant <SumantKushwaha.dev@gmail.com>
2025-05-02 17:27:14 +0530

    LazyColumn List and ListIndexed

---
+++ app/src/main/java/com/amigo/phillipcourse/Lists.kt
@@ -0,0 +1,49 @@
+package com.amigo.phillipcourse
+
+import androidx.compose.foundation.horizontalScroll
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun Lists() {
+    val scrollState = rememberScrollState()
+    LazyColumn {
+        itemsIndexed(
+            listOf("This","Is","Jetpack","Compose")
+        ){index,string->
+            Text(
+                text = string,
+                fontSize = 30.sp,
+                fontWeight = FontWeight.Bold,
+                textAlign = TextAlign.Center,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(vertical = 24.dp)
+            )
+        }
+//        items(3000) {
+//            Text(
+//                text = "Item $it",
+//                fontSize = 30.sp,
+//                fontWeight = FontWeight.Bold,
+//                textAlign = TextAlign.Center,
+//                modifier = Modifier
+//                    .fillMaxWidth()
+//                    .padding(vertical = 24.dp)
+//            )
+//        }
+    }
+}
