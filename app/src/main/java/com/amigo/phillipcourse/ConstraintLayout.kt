2081cf186f52e2a23ae244d255d3d7b8f9455ad2
Sumant <SumantKushwaha.dev@gmail.com>
2025-05-03 07:47:33 +0530

    Constraint Layout in Compose

---
+++ app/src/main/java/com/amigo/phillipcourse/ConstraintLayout.kt
@@ -0,0 +1,50 @@
+package com.amigo.phillipcourse
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.layoutId
+import androidx.compose.ui.unit.dp
+import androidx.constraintlayout.compose.ConstraintLayout
+import androidx.constraintlayout.compose.ConstraintSet
+import androidx.constraintlayout.compose.Dimension
+
+@Composable
+fun ConstrainLayout() {
+    val constraints = ConstraintSet {
+        val greenBox = createRefFor("greenbox")
+        val redBox = createRefFor("redbox")
+
+        constrain(greenBox) {
+            top.linkTo(parent.top)
+            start.linkTo(parent.start)
+            width = Dimension.value(400.dp)
+            height = Dimension.value(400.dp)
+        }
+
+        constrain(redBox) {
+            top.linkTo(greenBox.top)
+            start.linkTo(greenBox.start)
+            bottom.linkTo(greenBox.bottom)
+            end.linkTo(greenBox.end)
+            width = Dimension.value(200.dp)
+            height = Dimension.value(200.dp)
+        }
+    }
+    ConstraintLayout(constraints, modifier = Modifier.fillMaxSize()) {
+        Box(
+            modifier = Modifier
+                .background(Color.Green)
+                .layoutId("greenbox")
+        )
+        Box(
+            modifier = Modifier
+                .background(Color.Red)
+                .layoutId("redbox")
+        )
+
+    }
+}
