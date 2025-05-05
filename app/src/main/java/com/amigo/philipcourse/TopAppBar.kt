50989bd38cc355bdee6294e90e1e4c019448303a
Sumant <SumantKushwaha.dev@gmail.com>
2025-05-05 20:14:47 +0530

    Top Bar with menu Scaffold

--- app/src/main/java/com/amigo/philipcourse/TopAppBar.kt
+++ app/src/main/java/com/amigo/philipcourse/TopAppBar.kt
@@ -1,14 +1,16 @@
 package com.amigo.philipcourse
 
+import android.content.Context
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.FavoriteBorder
-import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.filled.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
@@ -18,7 +20,7 @@ import androidx.compose.ui.platform.LocalContext
 fun CreateScaffold() {
     Scaffold(
         topBar = { TopBarExample() }
-    ) {values->
+    ) { values ->
         IconsExample()
         Modifier.padding(values)
     }
@@ -26,14 +28,39 @@ fun CreateScaffold() {
 
 @Composable
 fun TopBarExample() {
-    val context= LocalContext.current
+    val showMenu= remember {
+        mutableStateOf(false)
+    }
+    val context = LocalContext.current
     TopAppBar(
         title = { Text(text = "Top App Bar") },
         navigationIcon = {
             IconButton(onClick = {
-                android.widget.Toast.makeText(context,"Menu Opened", android.widget.Toast.LENGTH_SHORT).show()
+                android.widget.Toast.makeText(
+                    context,
+                    "Menu Opened",
+                    android.widget.Toast.LENGTH_SHORT
+                ).show()
             }) {
                 Icon(Icons.Default.Menu, contentDescription = null)
+            }
+        },
+        actions = {
+            IconButton(onClick = {  }) {
+                Icon(Icons.Default.FavoriteBorder, contentDescription = null)
+            }
+            IconButton(onClick = {showMenu.value=true}) {
+                Icon(Icons.Default.MoreVert, contentDescription = null)                
+            }
+            DropdownMenu(expanded = showMenu.value, onDismissRequest = { showMenu.value = false }) {
+                DropdownMenuItem(onClick = { makeToast(context,"More Icons") }) {
+                    Icon(Icons.Default.Person, contentDescription = null)
+                    Text(text = "Person")
+                }
+                DropdownMenuItem(onClick = { makeToast(context,"More Icons") }) {
+                    Icon(Icons.Default.MailOutline, contentDescription = null)
+                    Text(text = "Mail")
+                }
             }
         }
     )
@@ -48,4 +75,9 @@ fun IconsExample() {
     ) {
         Icon(Icons.Default.FavoriteBorder, contentDescription = null)
     }
+}
+
+
+fun makeToast(conx:Context,msg:String){
+    android.widget.Toast.makeText(conx,msg, android.widget.Toast.LENGTH_SHORT).show()
 }
