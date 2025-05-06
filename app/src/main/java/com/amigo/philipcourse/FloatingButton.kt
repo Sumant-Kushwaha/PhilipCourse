23030bb5876e6a7e7a3fe0a4776f766b2037c38c
Sumant <SumantKushwaha.dev@gmail.com>
2025-05-06 17:23:05 +0530

    Floating Button with SnackBar Scaffold

--- app/src/main/java/com/amigo/philipcourse/FloatingButton.kt
+++ app/src/main/java/com/amigo/philipcourse/FloatingButton.kt
@@ -2,23 +2,24 @@ package com.amigo.philipcourse
 
 import android.widget.Toast.makeText
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.FabPosition
-import androidx.compose.material.FloatingActionButton
-import androidx.compose.material.Icon
-import androidx.compose.material.Scaffold
+import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import kotlinx.coroutines.launch
 
 
 @Composable
 fun RunFloatingButton() {
+    val scaffoldState = rememberScaffoldState()
     Scaffold(
-        floatingActionButton = { FloatingActionButtonExample() },
+        floatingActionButton = { FloatingActionButtonExample(scaffoldState) },
         floatingActionButtonPosition = FabPosition.Center,
 //        isFloatingActionButtonDocked = false
+        scaffoldState = scaffoldState
     ) { values ->
         Modifier.padding(values)
     }
@@ -26,11 +27,30 @@ fun RunFloatingButton() {
 
 
 @Composable
-fun FloatingActionButtonExample() {
+fun FloatingActionButtonExample(state: ScaffoldState) {
 
     val context = LocalContext.current
+    val coroutineScope = rememberCoroutineScope()
 
-    FloatingActionButton(onClick = { makeToast(context, "Floating Action \n Button Clicked") }) {
+    FloatingActionButton(onClick = {
+//        makeToast(context, "Floating Action \n Button Clicked")
+        coroutineScope.launch {
+            val result = state.snackbarHostState.showSnackbar(
+                message = "Fab Clicked",
+                duration = SnackbarDuration.Short,
+                actionLabel = "Action"
+            )
+            when (result) {
+                SnackbarResult.ActionPerformed -> {
+                    makeToast(context, "SnackBar Action Performed")
+                }
+                SnackbarResult.Dismissed -> {
+                    makeToast(context, "SnackBar Action Dismissed")
+                }
+                else -> {}
+            }
+        }
+    }) {
         Icon(Icons.Default.Add, contentDescription = null)
     }
 }
